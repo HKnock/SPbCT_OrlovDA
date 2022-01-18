2cf3c4c56f9dc25fd73dfa5724c5f9e0d3bac8f0
HKnock <lolu4ka@gmail.com>
2022-01-18 18:07:15 +0300

    ПЗ2

---
+++ PZ2/src/com/metanit/Main.java
@@ -0,0 +1,10 @@
+package com.metanit;
+
+public class Main {
+
+    public static void main(String[] args){
+        double d = 3.14;
+        int i = (int) d;
+        System.out.println(i);
+    }
+}
